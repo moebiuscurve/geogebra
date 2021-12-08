1e501c8e9fa786e902b050f6b6de630f905f1768
Laszlo Gal <laszlo@geogebra.org>
2021-12-08 10:41:11 +0100

    WLY-300 Collects dynamic altTexts, reads them

--- web-common/src/main/java/org/geogebra/web/html5/main/AltTextCollector.java
+++ web-common/src/main/java/org/geogebra/web/html5/main/AltTextCollector.java
@@ -1,21 +1,25 @@
 package org.geogebra.web.html5.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.geogebra.common.kernel.geos.GeoText;
 import org.geogebra.common.kernel.geos.ScreenReaderBuilder;
 import org.geogebra.common.main.App;
+import org.geogebra.common.main.Localization;
 import org.geogebra.web.html5.gui.accessibility.ViewAltTexts;
 
 public class AltTextCollector {
-	private final ScreenReaderBuilder sb;
-	private int count;
+	private final List<String> lines;
+	private final Localization loc;
 	private final AltTextTimer timer;
 	private final ViewAltTexts views;
 
 	public AltTextCollector(App app, ViewAltTexts views) {
 		this.views = views;
-		count = 0;
 		timer = new AltTextTimer(app.getActiveEuclidianView().getScreenReader());
-		sb = new ScreenReaderBuilder(app.getLocalization());
+		loc = app.getLocalization();
+		lines = new ArrayList<>();
 	}
 
 	public void add(GeoText altText) {
@@ -23,17 +27,23 @@ public class AltTextCollector {
 			return;
 		}
 
-		sb.append(altText.getAuralText());
-		sb.appendSpace();
+		lines.add(altText.getAuralText());
 		if (isLastAltText()) {
-			timer.read(sb);
-			count = 0;
-		} else {
-			count++;
+			timer.read(concatLines());
+			lines.clear();
 		}
 	}
 
+	private ScreenReaderBuilder concatLines() {
+		ScreenReaderBuilder sb = new ScreenReaderBuilder(loc);
+		for (String line: lines) {
+			sb.append(line);
+			sb.appendSpace();
+		}
+		return sb;
+	}
+
 	private boolean isLastAltText() {
-		return count == views.activeAltTextCount();
+		return lines.size() == views.activeAltTextCount();
 	}
 }
